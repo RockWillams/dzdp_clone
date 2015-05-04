27c8db3e8d5fee981259ea10a6a0e3ef8f4b5c3e
Danny Wu <wushuo1988@hotmail.com>
2015-05-03 20:32:28 -0400

    Associated user with shops when a new shop is created

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,30 +1,23 @@
 class ShopsController < ApplicationController
   before_action :set_shop, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /shops
-  # GET /shops.json
   def index
     @shops = Shop.all
   end
 
-  # GET /shops/1
-  # GET /shops/1.json
   def show
   end
 
-  # GET /shops/new
   def new
-    @shop = Shop.new
+    @shop = current_user.shops.build
   end
 
-  # GET /shops/1/edit
   def edit
   end
 
-  # POST /shops
-  # POST /shops.json
   def create
-    @shop = Shop.new(shop_params)
+    @shop = current_user.shops.build(shop_params)
 
     respond_to do |format|
       if @shop.save
@@ -51,8 +44,6 @@ class ShopsController < ApplicationController
     end
   end
 
-  # DELETE /shops/1
-  # DELETE /shops/1.json
   def destroy
     @shop.destroy
     respond_to do |format|
@@ -62,12 +53,10 @@ class ShopsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_shop
       @shop = Shop.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def shop_params
       params.require(:shop).permit(:title, :comment, :rating)
     end
