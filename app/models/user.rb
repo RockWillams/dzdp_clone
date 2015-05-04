83881a76ec61ce0b409d81210f89d4d6201dda73
Danny Wu <wushuo1988@hotmail.com>
2015-05-03 21:41:11 -0400

    Installed Boostrap sass gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
 
   has_many :shops
 
-  has_attached_file :image, styles: { medium: "400x600#"}
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
 
