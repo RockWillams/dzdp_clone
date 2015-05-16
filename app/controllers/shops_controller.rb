45eb7b570dba00dee7737c8f5f85264d53df88c3
Danny Wu <wushuo1988@hotmail.com>
2015-05-16 14:00:57 -0400

    Convert review ratings to stars

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -8,6 +8,12 @@ class ShopsController < ApplicationController
 
   def show
     @reviews = Review.where(shop_id: @shop.id).order("created_at DESC")
+
+    if @reviews.blank?
+      @avg_review = 0
+    else
+      @avg_review = @reviews.average(:rating).round(2)
+    end
   end
 
   def new
